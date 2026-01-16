ccf24e2c8fd039101f2ec208ffe99dfd7a6aea77
Szilvia Kuik <skuik@c2r1s5.42prague.com>
2026-01-16 15:01:37 +0100

    fixed split

--- lib/libft/ft_split.c
+++ lib/libft/ft_split.c
@@ -3,111 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: skuik <skuik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 19:15:48 by tomas             #+#    #+#             */
-/*   Updated: 2025/09/24 13:16:43 by tomas            ###   ########.fr       */
+/*   Updated: 2026/01/16 15:00:27 by skuik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-// static void free_words(char **word, int count)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (i < count)
-// 	{
-// 		free(word[i]);
-// 		i++;
-// 	}
-// 	free(word);
-// }
-
-static int	ar_mal(char **result, int location, size_t buffer)
+size_t	ft_wordcount(char const *s, char c)
 {
-	result[location] = malloc(buffer);
-	if (result[location] == NULL)
-		return (1);
-	return (0);
+	size_t	word;
+
+	if (!s)
+		return (0);
+	word = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+		{
+			word++;
+			while (*s != c && *s)
+				s++;
+		}
+	}
+	return (word + 1);
 }
 
-static int	wordscpy(char **result, char const *s, char c)
+size_t	ft_wordlen(char const *s, char c, int i)
 {
 	size_t	len;
-	int		i;
 
-	i = 0;
-	while (*s)
+	len = 0;
+	while (s[i] != c && s[i])
 	{
-		len = 0;
-		while (*s && *s == c)
-			++s;
-		while (*s && *s != c)
-		{
-			++len;
-			++s;
-		}
-		if (len)
-		{
-			if (ar_mal(result, i, len + 1))
-				return (1);
-			ft_strlcpy(result[i], s - len, len + 1);
-		}
+		len++;
 		i++;
 	}
-	return (0);
-}
-
-static size_t	count_words(char const *s, char c)
-{
-	size_t	words;
-	bool	state;
-
-	words = 0;
-	while (*s)
-	{
-		state = false;
-		while (*s && *s == c)
-			s++;
-		while (*s && *s != c)
-		{
-			if (!state)
-			{
-				words++;
-				state = true;
-			}
-			s++;
-		}
-	}
-	return (words);
+	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	words;
-	char	**result;
+	int		i;
+	int		j;
+	int		word;
+	int		len;
+	char	**str;
 
-	words = 0;
-	words = count_words(s, c);
-	result = ft_calloc(words + 1, sizeof(char *));
-	if (result == NULL)
+	i = 0;
+	j = -1;
+	word = ft_wordcount(s, c);
+	str = ft_calloc(word, sizeof(char *));
+	if (str == NULL)
 		return (NULL);
-	if (wordscpy(result, s, c))
-		return (NULL);
-	return (result);
+	while (++j < word - 1)
+	{
+		while (s[i] == c)
+			i++;
+		len = ft_wordlen(s, c, i);
+		str[j] = ft_substr(s, i, len);
+		i += len;
+	}
+	str[j] = NULL;
+	return (str);
 }
-
-// int main(void)
-// {
-// 	char *s = "  tripouille  42  ";
-// 	char **v = ft_split(s, ' ');
-// 	while (*v)
-// 	{
-// 		printf("%s\n", *v++);
-// 	}
-// }
